21f32d14f5d1a2bf144a3289f124daab4fb2cdfa
TrentWilli <98176473+TrentWilli@users.noreply.github.com>
2022-02-23 18:55:42 -0600

    getting somewhere

--- ForStudents/src/isa.h
+++ ForStudents/src/isa.h
@@ -81,18 +81,118 @@ int ADD (int Rd, int Rn, int Operand2, int I, int S, int CC) {
       NEXT_STATE.CPSR |= N_N;
     if (cur == 0)
       NEXT_STATE.CPSR |= Z_N;
+    if ((0x00000008 & cur))
+      NEXT_STATE.CPSR |= C_N;
   }	
   //add Overflow flag check
+
 
   return 0;
 
 }
 
 int ADC (int Rd, int Rn, int Operand2, int I, int S, int CC){
-  //add code
+  
+  int cur = 0;
+  if(I == 0) {
+    int sh = (Operand2 & 0x00000060) >> 5;
+    int shamt5 = (Operand2 & 0x00000F80) >> 7;
+    int bit4 = (Operand2 & 0x00000010) >> 4;
+    int Rm = Operand2 & 0x0000000F;
+    int Rs = (Operand2 & 0x00000F00) >> 8;
+    if (bit4 == 0) 
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] + 
+	  (CURRENT_STATE.REGS[Rm] << shamt5);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] + 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] + 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+    	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] + 
+	      ((CURRENT_STATE.REGS[Rm] >> shamt5) |
+               (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
+	  break;
+      }     
+    else
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] + 
+	  (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] + 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] + 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] + 
+	      ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) |
+               (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
+	  break;
+      }      
+  }
+  if (I == 1) {
+    int rotate = Operand2 >> 8;
+    int Imm = Operand2 & 0x000000FF;
+    cur = CURRENT_STATE.REGS[Rn] + (Imm>>2*rotate|(Imm<<(32-2*rotate)));
+  }
+  NEXT_STATE.REGS[Rd] = cur;
+  if (S == 1) {
+    if (cur < 0)
+      NEXT_STATE.CPSR |= N_N;
+    if (cur == 0)
+      NEXT_STATE.CPSR |= Z_N;
+    if ((0x00000008 & cur) == 1)
+      NEXT_STATE.CPSR |= C_N;
+  }	
+  //add Overflow flag check
+
+  return 0;
 }
 int AND (int Rd, int Rn, int Operand2, int I, int S, int CC){
-  //add code
+    int cur = 0;
+  if(I == 0) {
+    int sh = (Operand2 & 0x00000060) >> 5;
+    int shamt5 = (Operand2 & 0x00000F80) >> 7;
+    int bit4 = (Operand2 & 0x00000010) >> 4;
+    int Rm = Operand2 & 0x0000000F;
+    int Rs = (Operand2 & 0x00000F00) >> 8;
+    if (bit4 == 0) 
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] << shamt5);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+    	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] & 
+	      ((CURRENT_STATE.REGS[Rm] >> shamt5) |
+               (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
+	  break;
+      }     
+    else
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] & 
+	      ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) |
+               (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
+	  break;
+      }      
+  }
+  return 0;
 }
 
 int ASR (int Rd, int Rn, int Operand2, int I, int S, int CC){
